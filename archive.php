75da5439c899217e7516510455ba3efed2dd8f87
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2024-04-01 19:54:07 +0200

    oprava-titulku

--- archive.php
+++ archive.php
@@ -1,21 +1,25 @@
 <?php
-  get_header();
+get_header();
 
-  $current_term = get_queried_object();
-  if ( $current_term instanceof WP_Term ) {
-      $title = ucwords( $current_term->name );
-  } else {
-      $title = __("Archiv tutoriálů", "your-text-domain");
-  }
+$current_term = get_queried_object();
+if ($current_term instanceof WP_Term) {
+    $title = ucwords($current_term->name);
+} else {
+    $title = __("Archiv tutoriálů", "your-text-domain");
+}
 ?>
+
 <script>
     document.addEventListener("DOMContentLoaded", function() {
         var title = document.querySelector('.heading');
-        if ( title ) {
-            title.textContent = "<?php echo esc_js( apply_filters( 'weglot_translate_html_element', $title, 'title' ) ); ?>";
+        if (title) {
+            // Pass the translated title to Weglot for translation
+            var translatedTitle = "<?php echo esc_js(apply_filters('weglot_translate_html_element', $title, 'title')); ?>";
+            title.textContent = translatedTitle;
         }
     });
 </script>
+
 <article class="content">
     <?php
       if ( have_posts() ) {
